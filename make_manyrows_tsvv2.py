c5223f1c915f63dc0ca9852038786f8260cdb5ae
Naohiro Kaide <naohirokaide@ZEROnoMacBook.local>
2016-12-15 19:24:12 +0900

    cmdのスリム化、自作の例外処理での処理をしました。百万行で38秒かかりました。

--- make_manyrows_tsvv2.py
+++ make_manyrows_tsvv2.py
@@ -13,13 +13,29 @@ from StringIO import StringIO
 import click
 
 
+class SignalException(Exception):
+    def __init__(self, message):
+        super(SignalException, self).__init__(message)
+
+
 def do_exit(sig, stack):
-    raise Exception("Exiting")
+    raise SignalException("Exiting")
 
 
 def make_manyrows(
-        fpath, temp, rows, dt_iso_max, dt_iso_min,
-        date_iso_max, date_iso_min):
+        fpath="/vagrant/work/magori_kadai/kadai_2/kadai_v1.tsv",
+        rows=1000000,
+        dt_iso_max=u"2016/12/31 00:00:00",
+        dt_iso_min=u"2016/12/1 00:00:00",
+        date_iso_max=u"2016/12/31",
+        date_iso_min=u"2016/12/1"):
+    dt_iso_max = datetime.datetime.strptime(
+        dt_iso_max, u'%Y/%m/%d %H:%M:%S')
+    dt_iso_min = datetime.datetime.strptime(
+        dt_iso_min, u'%Y/%m/%d %H:%M:%S')
+    date_iso_max = datetime.datetime.strptime(
+        date_iso_max, u'%Y/%m/%d')
+    date_iso_min = datetime.datetime.strptime(date_iso_min, u'%Y/%m/%d')
     chunk = 500  # 分割ブロック
     delta = dt_iso_max - dt_iso_min
     date_delta = date_iso_max - date_iso_min
@@ -27,6 +43,9 @@ def make_manyrows(
     int_date_delta = (date_delta.days * 24 * 60 * 60) + date_delta.seconds
     # 日時の範囲で差をとり、その値をランダムに変更することで作成している。
     try:
+        fd, temp = tempfile.mkstemp()
+        os.close(fd)
+        os.chmod(temp, stat.S_IRWXU | stat.S_IROTH)
         fd = codecs.open(temp, "wb", "utf-8")
         fd.write(u"\t".join(
             ["int", "short", "long", "double", "bool",
@@ -76,15 +95,17 @@ def make_manyrows(
             # 1000000を越えるとStringIOを解放する。
         fd.write(output.getvalue())
         shutil.move(temp, fpath)
-    except Exception as e1:
-        print e1, "srgs"
+    except SignalException:
+        print 'Exiting'
+        if os.path.exists(temp):
+            os.remove(temp)
     finally:
         output.close()
         fd.close()
 
 
 @click.command()
-@click.argument('rows', type=int, default=100000000)
+@click.argument('rows', type=int, default=1000000)
 @click.option(
     '-f', '--fpath',
     default="/vagrant/work/magori_kadai/kadai_2/kadai_v1.tsv",
@@ -101,28 +122,12 @@ def cmd(rows, fpath, dt_iso_max, dt_iso_min,
     signal.signal(signal.SIGHUP, do_exit)
     signal.signal(signal.SIGTERM, do_exit)
 
-    fd, temp = tempfile.mkstemp()
-    os.close(fd)
-    os.chmod(temp, stat.S_IRWXU | stat.S_IROTH)
-    try:
-        dt_iso_max = datetime.datetime.strptime(
-            dt_iso_max, u'%Y/%m/%d %H:%M:%S')
-        dt_iso_min = datetime.datetime.strptime(
-            dt_iso_min, u'%Y/%m/%d %H:%M:%S')
-        date_iso_max = datetime.datetime.strptime(
-            date_iso_max, u'%Y/%m/%d')
-        date_iso_min = datetime.datetime.strptime(date_iso_min, u'%Y/%m/%d')
-        make_manyrows(
-            fpath, temp, rows, dt_iso_max, dt_iso_min,
-            date_iso_max, date_iso_min
-        )
-    except Exception as e2:
-        print e2
-    finally:
-        if os.path.exists(temp):
-            os.remove(temp)
-        e = datetime.datetime.now()
-        print str(e-s)
+    make_manyrows(
+        fpath, rows, dt_iso_max, dt_iso_min,
+        date_iso_max, date_iso_min
+    )
+    e = datetime.datetime.now()
+    print str(e-s)
 
 
 def main():
