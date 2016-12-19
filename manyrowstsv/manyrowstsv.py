3c6e17b1e44f73d13fd0fbe8b6c1f4118e759b93
Naohiro Kaide <naohirokaide@ZEROnoMacBook.local>
2016-12-19 15:57:49 +0900

    修正しました。あと、残りの課題について作成しました。setup等はまだしてません。

--- manyrowstsv/manyrowstsv.py
+++ manyrowstsv/manyrowstsv.py
@@ -7,15 +7,25 @@ import os
 import glob
 import logging
 import logging.handlers
-import stat
-import codecs
 import shutil
 import tempfile
 import random
 import datetime
 import string
 import click
-from itertools import islice
+import itertools
+
+
+def write_str_into_file(
+    iterable,
+    output_filename,
+):
+    with tempfile.NamedTemporaryFile(delete=False) as f:
+        for row in iterable:
+            f.write(row)
+        shutil.move(f.name, output_filename)
+    if os.path.exists(f.name):
+        os.remove(f.name)
 
 
 class SignalException(Exception):
@@ -27,11 +37,10 @@ def do_exit(sig, stack):
     raise SignalException("Exiting")
 
 
-class WriteRowsTsv(object):
+class TsvRowGenerator(object):
 
     def __init__(
         self, dt_iso_max, dt_iso_min, date_iso_max, date_iso_min,
-        filename, rows
             ):
         self.dt_iso_max = datetime.datetime.strptime(
             dt_iso_max, '%Y/%m/%d %H:%M:%S')
@@ -40,28 +49,28 @@ class WriteRowsTsv(object):
         self.date_iso_max = datetime.datetime.strptime(
             date_iso_max, '%Y/%m/%d')
         self.date_iso_min = datetime.datetime.strptime(
-            date_iso_min, u'%Y/%m/%d')
-        self.filename = filename
-        self.rows = rows
-
-    def iterows(self):
+            date_iso_min, '%Y/%m/%d')
         delta = self.dt_iso_max - self.dt_iso_min
         date_delta = self.date_iso_max - self.date_iso_min
-        int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
-        int_date_delta = (date_delta.days * 24 * 60 * 60) + date_delta.seconds
+        self.int_delta = (delta.days * 24 * 60 * 60) + delta.seconds
+        self.int_date_delta = (date_delta.days * 24 * 60 * 60) + \
+            date_delta.seconds
 
-        yield "\t".join(
-            ["int", "short", "long", "double", "bool",
-                "char", "utf8", "dt_iso8601", "date_iso8601"]) + "\n"
+    def iterows(self):
+        yield (
+            "\t".join(
+                ["int", "short", "long", "double", "bool",
+                    "char", "utf8", "dt_iso8601", "date_iso8601"]
+                ) + "\n")
         while 1:
             rdp = random.randint(0, (1 << 32) - 1)
-            random_second = rdp % int_delta
+            random_second = rdp % self.int_delta
             randomtime = self.dt_iso_min + datetime.timedelta(
                 seconds=random_second)
-            random_date_second = rdp % int_date_delta
+            random_date_second = rdp % self.int_date_delta
             randomdatetime = self.date_iso_min + datetime.timedelta(
                 seconds=random_date_second)
-            yield "\t".join(
+            yield ("\t".join(
                 [
                     str(rdp - (1 << 31)),
                     str((rdp >> 16) - (1 << 15)),
@@ -73,39 +82,22 @@ class WriteRowsTsv(object):
                         string.ascii_letters) + random.choice(
                         string.ascii_letters) + random.choice(
                         string.ascii_letters),
-                    "ごんた",
+                    u"ごんた".encode('utf-8'),
                     randomtime.strftime("%Y-%m-%d %H:%M:%S"),
                     randomdatetime.strftime("%Y-%m-%d"),
-                ]) + "\n"
-
-    def writerows(self):
-        try:
-            fpath = "/tmp/" + self.filename
-            fd, temp = tempfile.mkstemp()
-            os.close(fd)
-            os.chmod(temp, stat.S_IRWXU | stat.S_IROTH)
-            fd = codecs.open(temp, "wU", "utf-8")
-            for row in islice(self.iterows(), self.rows+1):
-                fd.write(row.decode("utf-8"))
-            shutil.move(temp, fpath)
-            fd.close()
-        except Exception as e2:
-            if os.path.exists(temp):
-                os.remove(temp)
-            fd.close()
-            raise e2
+                ]) + "\n")
 
 
 @click.command()
 @click.argument('rows', type=int, default=1000000)
 @click.option(
     '-f', '--filename',
-    default="kadai_1.tsv",
+    default="~/kadai_1.tsv",
     )
-@click.option('-D', '--dt-iso-max', default=u"2016/12/31 00:00:00")
-@click.option('-d', '--dt-iso-min', default=u"2016/12/1 00:00:00")
-@click.option('-T', '--date-iso-max', default=u"2016/12/31")
-@click.option('-t', '--date-iso-min', default=u"2016/12/1")
+@click.option('-D', '--dt-iso-max', default="2016/12/31 00:00:00")
+@click.option('-d', '--dt-iso-min', default="2016/12/1 00:00:00")
+@click.option('-T', '--date-iso-max', default="2016/12/31")
+@click.option('-t', '--date-iso-min', default="2016/12/1")
 def cmd(rows, filename, dt_iso_max, dt_iso_min,
         date_iso_max, date_iso_min):
     LOG_MANYROWSTSV = 'logging_warning.out'
@@ -114,17 +106,19 @@ def cmd(rows, filename, dt_iso_max, dt_iso_min,
     handler = logging.handlers.RotatingFileHandler(
         LOG_MANYROWSTSV, maxBytes=2000, backupCount=5,)
     my_logger.addHandler(handler)
-    wri = WriteRowsTsv(
-        dt_iso_max, dt_iso_min, date_iso_max, date_iso_min,
-        filename, rows
-    )
     s = datetime.datetime.now()
     print s + datetime.timedelta(0, 0, 0, 0, 0, 9)
     signal.signal(signal.SIGINT, do_exit)
     signal.signal(signal.SIGHUP, do_exit)
     signal.signal(signal.SIGTERM, do_exit)
     try:
-        wri.writerows()
+        write_str_into_file(
+            iterable=itertools.islice(
+                TsvRowGenerator(
+                    dt_iso_max, dt_iso_min, date_iso_max, date_iso_min,
+                ).iterows(), rows + 1),
+            output_filename=os.path.abspath(os.path.expanduser(filename)),)
+        print os.path.abspath(os.path.expanduser(filename))
     except SignalException as e1:
         my_logger.warning('%s: %s' % (e1, datetime.datetime.now()))
         logfiles = glob.glob('%s*' % LOG_MANYROWSTSV)
